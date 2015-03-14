d7c3de3f6b0285f734e3899f86236dcb1b1a94a4
Lucas <lucas@agtlucas.com>
2015-03-14 09:44:04 -0300

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,30 @@ const sassLoaders = [
   "autoprefixer-loader?browsers=last 2 version",
   "sass-loader?includePaths[]=" + path.resolve(__dirname, "./src"),
 ];
+
+const config = [
+  entry: {
+    app: ["./src/index"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+    ]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./build"),
+    publicPath: "/",
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css"),
+  ],
+  resolve: {
+    extensions: ["", ".js", ".sass", "scss"],
+    modulesDirectories: ["src", "node_modules"],
+  },
+];
