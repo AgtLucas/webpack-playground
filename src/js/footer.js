1ce20867a952ad7b9d94d8c03ea6f12ba7a77fa7
Lucas <lucas@agtlucas.com>
2015-03-14 13:02:57 -0300

    Adding footer.js

---
+++ src/js/footer.js
@@ -0,0 +1,15 @@
+const React = require('react');
+
+require('scss/modules/footer');
+
+const Footer = React.createClass({
+  render () {
+    return (
+      <footer divClass="main-footer">
+        Footer
+      </footer>
+    );
+  }
+});
+
+module.exports = Footer;
